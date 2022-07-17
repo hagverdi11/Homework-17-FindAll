40de8b803b664922debb764e4664cd0c56c191ed
hagverdi11 <hagverdiam@code.edu.az>
2022-07-18 02:53:42 +0400

    Added new homework table

---
+++ FindAll/FindAll/Models/MyList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindAll.Models
+{
+    public class MyList <T>
+    {
+        private T[] _arr;
+        public int Count { get; set; }
+
+        public MyList()
+        {
+            _arr = new T[10];
+        }
+
+        public void Add (T data)
+        {
+            if (Count != 0 && Count % 10 == 0)
+            {
+                Array.Resize(ref _arr, _arr.Length + 10);
+            }
+            _arr[Count] = data;
+            Count++;
+        }
+
+        public void GetAll()
+        {
+            foreach (var item in _arr)
+            {
+                Console.WriteLine(item);
+
+            }
+        }
+
+        public void Clear()
+        {
+            _arr = new T[10];
+        }
+
+
+        public T Find (Predicate<T> predicate)
+        {
+            foreach (var item in _arr)
+            {
+                if (predicate(item))
+                {
+                    return item;
+                }
+
+            }
+            return default;
+        }
+
+
+        public T FindAll(Predicate<T> predicate)
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                if (predicate(_arr[i]))
+                {
+                    Console.WriteLine(_arr[i]);
+                }
+            }
+            return default;
+        }
+    }
+}
---
+++ FindAll/FindAll/Program.cs
@@ -0,0 +1,38 @@
+﻿using FindAll.Models;
+using System;
+
+namespace FindAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MyList<int> nums = new MyList<int>();
+            //nums.Add(1);
+            //nums.Add(2);
+            //nums.Add(3);
+            //nums.Add(4);
+            //nums.Add(5);
+            //nums.Add(6);
+            //nums.Add(7);
+            //nums.Add(8);
+            //nums.Add(9);
+            //nums.Add(10);
+            //nums.Add(11);
+
+
+            MyList<string> names = new MyList<string>();
+            names.Add("Orxan");
+            names.Add("Eli");
+            names.Add("Veli");
+            names.Add("Eli");
+
+
+            var result = names.Find(m => m == "Orxan");
+            Console.WriteLine(result);
+
+            var result1 = names.FindAll(m => m == "Eli");
+            Console.WriteLine(result1);
+        }
+    }
+}
